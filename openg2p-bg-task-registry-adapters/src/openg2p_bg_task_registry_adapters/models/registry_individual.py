a6c36186a787140e32bfcde4e280fd53dffeadde
RohailAkthar <aktharohail@gmail.com>
2026-01-17 15:42:57 +0530

    changes fixes

--- openg2p-bg-task-registry-adapters/src/openg2p_bg_task_registry_adapters/models/registry_individual.py
+++ openg2p-bg-task-registry-adapters/src/openg2p_bg_task_registry_adapters/models/registry_individual.py
@@ -1,10 +1,14 @@
 from openg2p_pbms_models.models import G2PRegistry
-from sqlalchemy import Date, String
-from sqlalchemy.orm import mapped_column
+from sqlalchemy import Date, String, cast
+from sqlalchemy.orm import mapped_column, column_property
 
 
 class G2PIndividualRegistry(G2PRegistry):
     __tablename__ = "res_partner"
+
+    # Safe alias for 'id' as 'link_registry_id' is not a separate column in res_partner
+    # Safe alias for 'id' as 'link_registry_id' is not a separate column in res_partner
+    link_registry_id = column_property(cast(id, String))
 
     name = mapped_column(String, nullable=False)
     gender = mapped_column(String, nullable=True)  # 'male' or 'female'
