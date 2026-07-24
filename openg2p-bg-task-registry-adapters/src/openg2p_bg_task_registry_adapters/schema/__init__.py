9534b31b5d8aa69b925a5d3fc349913873f18f0f
RohailAkthar <aktharohail@gmail.com>
2026-07-25 01:10:17 +0530

    customised based on nigeria

--- openg2p-bg-task-registry-adapters/src/openg2p_bg_task_registry_adapters/schema/__init__.py
+++ openg2p-bg-task-registry-adapters/src/openg2p_bg_task_registry_adapters/schema/__init__.py
@@ -13,4 +13,7 @@ from .beneficiary_list_summary_student import (
 from .registry import G2PRegistryPayload
 from .registry_farmer import G2PFarmerRegistryPayload
 from .registry_student import G2PStudentRegistryPayload
-from .registry_household import G2PHouseholdRegistryPayload
+from .registry_household import (
+    BeneficiaryListSummaryHousehold,
+    G2PHouseholdRegistryPayload,
+)
